e53a3357973559487e16437654dc1dfb482536e9
Pascal Lefebvre <skrova2017@gmail.com>
2023-04-19 15:08:26 +0200

    Update URLs compatible with the new apps.
    - Create a namespace for each app
    - Update path to template file in views functions
    - Update url parameter in template files

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from . import views
+from oc_lettings_site import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("lettings/", include("lettings.urls")),
+    path("profiles/", include("profiles.urls")),
+    path("admin/", admin.site.urls),
 ]
+
+# urlpatterns = [
+#     path("", views.index, name="index"),
+#     path("lettings/", lettings_views.index, name="lettings_index"),
+#     path("lettings/<int:letting_id>/", lettings_views.letting, name="letting"),
+#     path("profiles/", profiles_views.index, name="profiles_index"),
+#     path("profiles/<str:username>/", profiles_views.profile, name="profile"),
+#     path("admin/", admin.site.urls),
+# ]
