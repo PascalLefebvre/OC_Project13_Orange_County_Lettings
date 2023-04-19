f2091cefdf4e3718e123cc0ea44ad74b9290ad23
Pascal Lefebvre <skrova2017@gmail.com>
2023-04-19 16:29:21 +0200

    Convert "oc_lettinfs_site" into an app.
    - Create the "config" directory to locate project configuration files

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "oc_lettings_site.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -61,7 +61,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "oc_lettings_site.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
@@ -103,7 +103,8 @@ TIME_ZONE = "CET"
 
 USE_I18N = True
 
-USE_L10N = True
+# Depreciated
+# USE_L10N = True
 
 USE_TZ = True
 
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,20 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from oc_lettings_site import views
 
+app_name = "oc_lettings_site"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("lettings/", include("lettings.urls")),
-    path("profiles/", include("profiles.urls")),
-    path("admin/", admin.site.urls),
 ]
-
-# urlpatterns = [
-#     path("", views.index, name="index"),
-#     path("lettings/", lettings_views.index, name="lettings_index"),
-#     path("lettings/<int:letting_id>/", lettings_views.letting, name="letting"),
-#     path("profiles/", profiles_views.index, name="profiles_index"),
-#     path("profiles/<str:username>/", profiles_views.profile, name="profile"),
-#     path("admin/", admin.site.urls),
-# ]
